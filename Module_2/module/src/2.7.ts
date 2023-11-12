93c6bfdee31792e91f9202b51a411bb48247c057
Shafayathub <shafayat.ms@gamilcom>
2023-11-13 01:59:51 +0600

    practicing

--- Module_2/module/src/2.7.ts
+++ Module_2/module/src/2.7.ts
@@ -1,5 +1,39 @@
 {
   //
-  // Basic Promise
+  // Promise
+  type Todo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+  const dataFromAPI = async (): Promise<Todo> => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+  dataFromAPI();
+
+  const createPromise = (): Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
+      const data: string = "Some data";
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Failed to load data");
+      }
+    });
+  };
+
+  // Calling createPromise
+  const callingPromise = async (): Promise<string> => {
+    const data: string = await createPromise();
+    // console.log(data);
+    return data;
+  };
+  callingPromise();
   //
 }
