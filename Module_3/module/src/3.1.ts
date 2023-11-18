a3be0216b3c74bdd662a96a1ea56aa37c1bad00d
Shafayathub <shafayat.ms@gamilcom>
2023-11-18 14:40:20 +0600

    updated

--- Module_3/module/src/3.1.ts
+++ Module_3/module/src/3.1.ts
@@ -1,5 +1,32 @@
 {
   //
-  const a: number = 3;
+  // OOP --- Class
+  class Animal {
+    // public name: string;
+    // public species: string;
+    // public sound: string;
+
+    // public modifier define & initialize properties
+
+    // Parameter Properties
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {}
+    //  {
+    // this.name = name;
+    // this.species = species;
+    // this.sound = sound;
+    // }
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`);
+    }
+  }
+  const duck = new Animal("Duck", "Duck", "Pek pek pekao");
+  const cat = new Animal("Persian", "Cat", "Meaw Meaw");
+
+  duck.makeSound();
+  cat.makeSound();
   //
 }
